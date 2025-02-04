f0ac120f8927e65f213050b5713a40b6f0a7f97d
JosephAaronMusara <josephaaronmusara@gmail.com>
2025-02-04 09:28:28 +0200

    routes remove unnecessary code

--- routes/SubmoduleRoutes.js
+++ routes/SubmoduleRoutes.js
@@ -1,11 +1,9 @@
 const express = require('express');
 const SubModuleRouter = express.Router();
-// const { getAllSubModules, getSubModuleById, updateSubModule, deleteSubModule } = require('../controllers/SubmoduleController');
 const upload = require('../config/multer');
 const SubModuleR = require('../controllers/SubmoduleMainControler');
 const uploadSubModuleRouter = require('../controllers/SubmoduleController'); // Import the upload route
 
-// Existing routes
 SubModuleRouter.get('/api/submodules', SubModuleR.getAllSubModules);
 SubModuleRouter.get('/api/submodules/:id', SubModuleR.getSubModuleById);
 SubModuleRouter.put('/api/submodules/:id', SubModuleR.updateSubModule);
