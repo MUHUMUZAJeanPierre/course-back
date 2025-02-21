23369e436612b62399f100047fcd32fea6936cac
MUHUMUZAJeanPierre <j.muhumuza@alustudent.com>
2025-02-21 11:51:29 +0200

    feat

--- config/passport.js
+++ config/passport.js
@@ -1,52 +1,81 @@
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const User = require('../models/User');
-const { sendVerificationEmail } = require('../service/emailService');
-const crypto = require('crypto');
-const dotenv = require('dotenv');
+// const passport = require('passport');
+// const GoogleStrategy = require('passport-google-oauth20').Strategy;
+// const User = require('../models/User');
+// const { sendVerificationEmail } = require('../service/emailService');
+// const crypto = require('crypto');
+// const dotenv = require('dotenv');
+// dotenv.config();
+
+
+// passport.use(
+//     new GoogleStrategy(
+//         {
+//             clientID: process.env.GOOGLE_CLIENT_ID,
+//             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+//             callbackURL: "https://course-back-2-00rq.onrender.com/auth/google/callback",
+            
+//         },
+//         async (accessToken, refreshToken, profile, done) => {
+//             try {
+//                 let user = await User.findOne({ googleId: profile.id });
+
+//                 if (!user) {
+//                     const token = crypto.randomBytes(32).toString('hex');
+//                     const newUser = new User({
+//                         googleId: profile.id,
+//                         username: profile.displayName,
+//                         email: profile.emails[0].value,
+//                         password: crypto.randomBytes(16).toString('hex'),
+//                         isVerified: false,
+//                         verificationToken: token,
+//                     });
+
+//                     await newUser.save();
+//                     await sendVerificationEmail(newUser.email, token);
+
+//                     return done(null, newUser);
+//                 }
+
+//                 if (!user.isVerified) {
+//                     return done(null, false, { message: 'Please verify your email to continue' });
+//                 }
+
+//                 return done(null, user);
+//             } catch (error) {
+//                 console.error('Error during OAuth authentication:', error);
+//                 return done(error);
+//             }
+//         }
+//     )
+// );
+
+// passport.serializeUser((user, done) => done(null, user));
+// passport.deserializeUser((user, done) => done(null, user));
+
+// module.exports = passport;
+
+
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const dotenv = require("dotenv");
 dotenv.config();
 
-
 passport.use(
-    new GoogleStrategy(
-        {
-            clientID: process.env.GOOGLE_CLIENT_ID,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-            callbackURL: "https://course-back-2-00rq.onrender.com/auth/google/callback",
-            
-        },
-        async (accessToken, refreshToken, profile, done) => {
-            try {
-                let user = await User.findOne({ googleId: profile.id });
-
-                if (!user) {
-                    const token = crypto.randomBytes(32).toString('hex');
-                    const newUser = new User({
-                        googleId: profile.id,
-                        username: profile.displayName,
-                        email: profile.emails[0].value,
-                        password: crypto.randomBytes(16).toString('hex'),
-                        isVerified: false,
-                        verificationToken: token,
-                    });
-
-                    await newUser.save();
-                    await sendVerificationEmail(newUser.email, token);
-
-                    return done(null, newUser);
-                }
-
-                if (!user.isVerified) {
-                    return done(null, false, { message: 'Please verify your email to continue' });
-                }
-
-                return done(null, user);
-            } catch (error) {
-                console.error('Error during OAuth authentication:', error);
-                return done(error);
-            }
-        }
-    )
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: "https://course-back-2-00rq.onrender.com/auth/google/callback",
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      try {
+        return done(null, profile);
+      } catch (error) {
+        console.error("Error during Google authentication:", error);
+        return done(error);
+      }
+    }
+  )
 );
 
 passport.serializeUser((user, done) => done(null, user));
